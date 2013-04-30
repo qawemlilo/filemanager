51a9b109a9fe7cc0cae2d0604b57088dccf97f81
Qawelesizwe <qawemlilo@gmail.com>
2013-04-30 14:55:23 +0200

    ...

--- mod_ordernow/tmpl/default.php
+++ mod_ordernow/tmpl/default.php
@@ -6,20 +6,23 @@ defined('_JEXEC') or die('Restricted access'); // no direct access
 <a href="#myModal" style="margin-top:5px; color:#fff" role="button" class="btn btn-large btn-warning" data-toggle="modal">Order Now</a>
      
     <!-- Modal -->
-<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div id="myModal" style="width:260px; text-align: left; margin-left: -130px" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
     <h3 id="myModalLabel">Order Form</h3>
   </div>
   
-  <div class="modal-body">
-    <form method="post" name="contactusform"  id="contactusform">
+  <form method="post" name="contactusform"  id="contactusform">
+  <div class="modal-body"> 
        <div class="progress progress-striped active" style="display:none">
          <div class="bar" style="width: 100%;"></div>
        </div>
-       
+ 
        <div id="responseD" class="alert" style="display:none">
        </div>
+       
+       
+       <p>Fill in this form and a download link will be email to you.</p>
          
        <div class="control-group">
          <label class="control-label" for="name"> <i class="icon-user"> </i> Name</label>
@@ -36,24 +39,18 @@ defined('_JEXEC') or die('Restricted access'); // no direct access
            <input type="text" style="margin-left: 0px" required="" placeholder="Your email address" name="email">
          </div>
        </div>
-       
-       <div class="control-group">
-         <label class="control-label" for="message"> <i class="icon-pencil"> </i> Message </label>
-        
-         <div class="controls">
-           <textarea style="margin-left: 0px" required="" placeholder="Your message..." name="message" rows="3"></textarea>
-         </div>
-       </div>
-    </form>
+       <?php echo JHtml::_('form.token'); ?>
   </div>
   
-  <div class="modal-footer">
+  <div class="modal-footer" style="text-align: left">
+    <button type="submit" class="btn btn-primary">Order Now</button>
     <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-    <button class="btn btn-primary">Order Now</button>
   </div>
+  </form>
 </div>
 
 <script>window.jQuery || document.write('<script src="<?php echo JURI::base() . 'modules/mod_ordernow/asserts/js/jquery.js' ?>"><\/script>')</script>
+<script src="<?php echo JURI::base() . 'modules/mod_ordernow/asserts/js/bootstrap-transition.js'; ?>"></script>
 <script src="<?php echo JURI::base() . 'modules/mod_ordernow/asserts/js/bootstrap-modal.js'; ?>"></script>
 <script>
 jQuery.noConflict();
@@ -61,6 +58,42 @@ jQuery.noConflict();
 (function ($) {
     $('#myModal').modal({
         show: false
+    });
+    
+    
+    $("#contactusform").on('submit', function () {   
+        var self = this,
+            progress = $('.progress'),
+            responseD = $('#responseD');
+            
+        progress.slideDown('slow', function () {
+            $.post('index.php?option=filemanager&task=orders.order', $(self).serialize())
+            .done(function(data) {
+                progress.slideUp('slow', function () {
+                    responseD.addClass('alert-success').html($('<strong>Order sent!</strong>')).slideDown('slow');
+                });
+                  
+                window.setTimeout(function () { 
+                    responseD.slideUp('slow', function () {
+                        responseD.removeClass('alert-success');
+                        $('#myModal').modal('hide');
+                    }); 
+                }, 10 * 1000);
+            })
+            .fail(function(data) {
+                progress.slideUp('slow', function () {
+                    responseD.addClass('alert-error').html($('<strong>Error, Order not sent!</strong>')).slideDown();
+                });
+                  
+                window.setTimeout(function () { 
+                    responseD.slideUp('slow', function () {
+                        responseD.removeClass('alert-error');
+                    }); 
+                }, 10 * 1000);
+            });
+        });
+            
+            return false;
     });
 })(jQuery);
 
